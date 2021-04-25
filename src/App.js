eb44c3ff190cb4baa030a1191b4ddb41863941bb
Archana Nagaraja <johndoe@example.com>
2021-04-25 07:43:01 -0700

    added contact and renederd in app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Contact from './components/Contact';
 import About from './components/About';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Contact></Contact>
         <main>
             <About></About>
         </main>
---
+++ src/components/Contact/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import { validateEmail } from '../../utils/helper';
+
+function Contact() {
+    return (
+        <section className="my-5">
+        <h1 id="about">Contact Me</h1>
+       
+        </section>
+    );
+  }
+
+export default Contact;
