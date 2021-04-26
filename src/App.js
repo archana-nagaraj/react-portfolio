11d34ea7e63e68f021705be4525f6a5352872ef9
Archana Nagaraja <johndoe@example.com>
2021-04-25 18:53:49 -0700

    code to render the app content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import Contact from './components/Contact';
-import About from './components/About';
-import './App.css';
+import React, { useState } from "react";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import ContactForm from "./components/Contact";
+import Header from "./components/Header";
+import Resume from"./components/Resume";
+import Footer from './components/Footer'
 
 function App() {
+  const [tabs] = useState([
+    { name: "about me" },
+    { name: "portfolio" },
+    { name: "contact" },
+    { name: "resume" },
+  ]);
+
+  const [currentTab, setCurrentTab] = useState(tabs[0]);
+
   return (
     <div>
-      <Contact></Contact>
-        <main>
-            <About></About>
-        </main>
+      <Header currentTab={currentTab} setCurrentTab={setCurrentTab} tabs={tabs} />
+      <main>
+        {currentTab.name === "about me" && <About />}
+        {currentTab.name === "portfolio" && <Portfolio />}
+        {currentTab.name === "contact" && <ContactForm />}
+        {currentTab.name === "resume" && <Resume />}
+      </main>
+      <Footer />
     </div>
   );
 }
